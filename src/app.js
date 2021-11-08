0d061e08a0d03b59dfc857a20abf1cd396e3cafb
AjiteshMahalingam <69707961+AjiteshMahalingam@users.noreply.github.com>
2021-11-08 13:31:50 +0530

    Day 1 Progress - Final
    Co-Authored-By: deek-15 <62826735+deek-15@users.noreply.github.com>
    Co-Authored-By: hemanthd2805 <87944106+hemanthd2805@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -17,10 +17,58 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) => {
   res.render('index');
 });
+// POST /contact -> handles the form data and forward it to mail
+app.post("/contact", (req, res) => {
+  // Mail function
+  res.redirect('/');
+});
+
+//GET /browse
+//GET /browse
+//GET /register/student
+//GET /login/student
+//GET /login/admin
+//POST /register/student
+//POST /login/student
+//POST /login/admin
+
+//[GET] /adminDashboard ::  admin landing page after login
+//[GET] /student :: student landing page after login
+//[GET] /student/profile :: view student profile
+//[GET] /student/bidder/browse
+//[GET] /student/seller
+//[UPDATE] /student/profile 
+
+
 
 app.listen(port, () => {
   console.log("Server is up on port : " + port);
 });
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'contact.auctionport@gmail.com',
+    pass: 'YrtHFiTinUfK5u7'
+  }
+});
+
+var mailOptions = {
+  from: 'contact.auctionport@gmail.com',
+  to: 'contact.auctionport@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
 
 // POST /contact -> handles the form data and forward it to mail
 
