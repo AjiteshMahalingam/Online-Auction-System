9cc9b17cb107c128fa43be5a0c75d5bda7ac665c
AjiteshMahalingam <69707961+AjiteshMahalingam@users.noreply.github.com>
2022-01-07 11:21:00 +0530

    Update connectDB

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,10 +1,11 @@
 const Mongoose = require('mongoose');
 
-Mongoose.connect('mongodb://127.0.0.1/auction-port', {
+Mongoose.connect( 'mongodb+srv://Torvus:Aji1407MERN@cluster-1.bwhtr.mongodb.net/Auction_DB?retryWrites=true&w=majority', {
     useNewUrlParser : true,
     useUnifiedTopology: true
 });
 
 // C:\Users\Home\mongodb\bin\mongod.exe --dbpath=C:\Users\Home\mongodb-data
+// mongodb://127.0.0.1/auction-port
 // mongodb://127.0.0.1/task-manager-api
 // mongodb+srv://Torvus:Aji1407MERN@cluster-1.bwhtr.mongodb.net/Auction_DB?retryWrites=true&w=majority
